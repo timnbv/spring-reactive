46e747af1e3b2dc06ea6782a04b580378051ebef
Tim Ryzhov <ryzhovtv@gmail.com>
2017-08-01 01:40:10 +0400

    Initial commit

--- customer-service/src/main/java/pl/piomin/services/customer/repository/CustomerRepository.java
+++ customer-service/src/main/java/pl/piomin/services/customer/repository/CustomerRepository.java
@@ -1,15 +1,14 @@
 package pl.piomin.services.customer.repository;
 
-import static org.springframework.data.mongodb.core.query.Criteria.where;
-import static org.springframework.data.mongodb.core.query.Query.query;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
 import org.springframework.stereotype.Repository;
-
 import pl.piomin.services.customer.model.Customer;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
 
 @Repository
 public class CustomerRepository {
--- customer-service/src/test/java/pl/piomin/services/customer/CustomerTest.java
+++ customer-service/src/test/java/pl/piomin/services/customer/CustomerTest.java
@@ -1,29 +1,23 @@
 package pl.piomin.services.customer;
 
 import java.util.logging.Logger;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import pl.piomin.services.common.Customer;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CustomerTest {
 
 	private static final Logger logger = Logger.getLogger("CustomerTest");
 
 	private WebClient webClient;
 
-	@LocalServerPort
-	private int port;
+
+	private int port = 8088;
 
 	@Before
 	public void setup() {
@@ -32,8 +26,10 @@ public class CustomerTest {
 
 	@Test
 	public void getCustomerAccounts() {
+
 		Customer customer = this.webClient.get().uri("/customer/accounts/234543647565")
-				.accept(MediaType.APPLICATION_JSON).exchange().then(response -> response.bodyToMono(Customer.class))
+				.accept(MediaType.APPLICATION_JSON).exchange()
+				.flatMap(clientResponse -> clientResponse.bodyToMono(Customer.class))
 				.block();
 		logger.info("Customer: " + customer);
 	}
@@ -42,7 +38,9 @@ public class CustomerTest {
 	public void addCustomer() {
 		Customer customer = new Customer(null, "Adam", "Kowalski", "123456787654");
 		customer = webClient.post().uri("/customer").accept(MediaType.APPLICATION_JSON)
-				.exchange(BodyInserters.fromObject(customer)).then(response -> response.bodyToMono(Customer.class))
+				.body(BodyInserters.fromObject(customer))
+				.exchange()
+				.flatMap(clientResponse -> clientResponse.bodyToMono(Customer.class))
 				.block();
 		logger.info("Customer: " + customer);
 	}
